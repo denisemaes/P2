f47c8cbf3ec1d82a46189a3f3b2f51446bb969e5
denisevandenberg <denisevandenberg@outlook.com>
2017-03-13 09:21:18 +0100

    Add files via upload

--- Upseiling.py
+++ Upseiling.py
@@ -21,49 +21,56 @@ Grey            = (192,192,192)
 class Game:
     def __init__(self):
         self.FPS = 30
-        self.clock = pygame.time.Clock()
+        self.Clock = pygame.time.Clock()
         self.Width = 1500
         self.Height = 750
         self.Display = pygame.display.set_mode((self.Width, self.Height))
-        self.terminate = False
+        self.Terminate = False
         self.Level = "menu"
             
     def Draw(self, image_destination): 
         self.Load = pygame.image.load(image_destination)
         self.Display.blit(self.Load, (0,0))
+        #fix dit, niet elke keer laden  >> Geen idee hoe?
 
     def Update(self): 
         pygame.display.update()
-        self.clock.tick(self.FPS)
+        self.Clock.tick(self.FPS)
 
     def Loop(self): 
-        while not self.terminate:
+        while not self.Terminate:
             if self.Level == "menu": 
                 for event in pygame.event.get(): 
                     if event.type == pygame.QUIT: 
-                        self.terminate = True 
+                        self.Terminate = True 
                 self.Draw("background_game_menu.jpg")
+                startbutton.Draw() ##Start button niet defined??????
                 self.Update() 
-            else: self.Exit = True 
+            else: self.Terminate = True 
 
 class Button:
     def __init__(self, x, y, w, h, image, constant, action):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.image = pygame.image.load(image)
-        self.constant = constant
-        self.pressed = False
-        self.pressing = False
-        self.action = action
+        self.X = x
+        self.Y = y
+        self.W = w
+        self.H = h
+        self.Image = pygame.image.load(image)
+        self.Constant = constant
+        self.Pressed = False
+        self.Pressing = False
+        self.Action = action
 
-    def rollover(self):
-        if self.x + self.w > pygame.mouse.get_pos()[0] > self.x and self.y + self.h >  pygame.mouse.get_pos()[1] > self.y:
-            self.draw()
+    def Draw(self):
+        # op dit moment wordt de knop altijd getekent. zorg ervoor dat er getekent wordt als de muis er overheen gaat. >> Geen idee hoe.
+        game.Display.blit(self.image, [0, 0]) # positie [0, 0], want kijk naar de images die je gebruikt. de button is niet rechtsbovenin getekent.   << Got it. Maar als dat wel het geval zou zijn dan wel zoals eerst?
 
-    def draw(self):
-            game.Display.blit(self.image, [self.x, self.y])
+    def Rollover(self):
+        if self.X + self.W > pygame.mouse.get_pos()[0] > self.X and self.Y + self.H >  pygame.mouse.get_pos()[1] > self.Y:
+            self.Draw() 
+            # dit is een verkeerde manier van programmeren. aangezien de draw functie iedere keer uitgevoert wordt, en niet rollover.   >>> Waarom is dat fout?
+            # stel je voert rollover uit in de draw functie, dan zou er infinite recursion komen als de if in rollover true is.   >>>> Is dat een slecht iets of een goed iets?? 
+
+
 
 
 class Square:
@@ -96,19 +103,19 @@ class Tower:
 class Player:
     def __init__(self):
         self.Square = 0
-        self.image = image
+        self.Image = image
     def SetPosition(self, number):
         self.Square = self.Square + number
 # ------------------------------------------------------------------ FUNCTIONS ------------------------------------------------------------#
 
 
-def terminate():
+def Terminate():
     pygame.quit()
     quit()
 
 # ------------------------------------------------------------------ RUNNING ------------------------------------------------------------#
 
 game = Game()
-startbutton = Button(40, 325, 235, 120, "background_game_menu_button1.png", False, game.Loop())
 game.Loop()
-terminate()
+startbutton = Button(40, 325, 235, 120, "background_game_menu_button1.png", False, game.Level == 'exit') #Dit was om puur iets erin te hebben, is dit beter? Zodat die straks naar ander level kan?
+Terminate()
